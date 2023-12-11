36fb8d4bf0db1773e166b9d9fd34eb428e08d0fb
atharva <atharva@truscholar.io>
2023-12-12 01:13:08 +0530

    added storage and api stacks

--- stacks/StorageStack.ts
+++ stacks/StorageStack.ts
@@ -1,8 +1,15 @@
-import { StackContext, Bucket } from 'sst/constructs';
+import { StackContext, Bucket, Table } from 'sst/constructs';
 
 export function StorageStack({ stack }: StackContext) {
-  // Create the S3 bucket
   const bucket = new Bucket(stack, 'Uploads');
 
-  return { bucket };
+  const table = new Table(stack, 'Notes', {
+    fields: {
+      userId: 'string',
+      noteId: 'string',
+    },
+    primaryIndex: { partitionKey: 'userId', sortKey: 'noteId' },
+  });
+
+  return { bucket, table };
 }
